1caa2b78f20311babf1a6616c05d98efb783211e
kgrymd <kgroymd@gmail.com>
2023-09-28 20:01:22 +0900

    svg icon size up

--- src/components/FooterTabBar.js
+++ src/components/FooterTabBar.js
@@ -41,7 +41,10 @@ const FooterTabBar = ({ user }) => {
                             viewBox="0 0 24 24"
                             strokeWidth={2}
                             stroke="currentColor"
-                            className="w-6 h-6">
+                            // className="w-6 h-6"
+                            height="calc(1.5rem * 2)" // Todo: 後でプッシュ
+                        // width="calc(1.5rem * 1.2)"
+                        >
                             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
                         </svg>
 
