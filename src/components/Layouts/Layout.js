c07a7c4d213501437c48b2b124717c4afc398177
kgrymd <kgroymd@gmail.com>
2023-10-02 19:11:59 +0900

    Remove unnecessary comments

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Layout = ({ children, bgc = 'rgba(243, 244, 246, 1)' }) => {
     return (
-        // <div className="min-h-screen bg-gray-100 h-full">
         <div style={{
             minHeight: '100vh',
-            // backgroundColor: '#c9cad4',
             backgroundColor: bgc,
             height: '100%'
         }}>
